3e0043c23a96cbe67bb6f82832acbfe787715d23
suyogsubedi <subedisuyog555@gmail.com>
2021-11-24 15:40:16 +0545

    Registration

---
+++ members/forms.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+from django.forms.widgets import EmailInput, TextInput
+
+
+# Inheriting from from UserCreationForm
+class RegisterUserForm(UserCreationForm):
+    email = forms.EmailField(widget=EmailInput(attrs={'class':'form-control'}))
+    first_name = forms.CharField(max_length=75,widget=TextInput(attrs={'class':'form-control'}))
+    last_name = forms.CharField(max_length=75,widget=TextInput(attrs={'class':'form-control'}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name','last_name','email','password1','password2')
+    
+    def __init__(self,*args,**kwargs):
+        super(RegisterUserForm,self).__init__(*args,**kwargs)
+        self.fields['username'].widget.attrs['class']= 'form-control'
+        self.fields['password1'].widget.attrs['class']= 'form-control'
+        self.fields['password2'].widget.attrs['class']= 'form-control'
--- static/scripts/facts.js
+++ static/scripts/facts.js
@@ -1,16 +1,14 @@
 fact = document.querySelector(".fact");
 fetch("http://numbersapi.com/random/year?json")
   .then((response) => {
-    // console.log("resolved", response);
+    console.log("resolved", response);
     // Gets the json data
     return response.json();
   })
   .then((data) => {
     console.log(data);
-    // document.write(data.text);
     fact.innerHTML = data.text;
   })
-
   .catch((err) => {
     console.log("rejected", err);
   });
