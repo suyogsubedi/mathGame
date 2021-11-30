f5215304dc4e4d0de232fc0bb85becc73308c266
suyogsubedi <subedisuyog555@gmail.com>
2021-11-30 22:32:51 +0545

    Subtraction and multiplication

--- website/views.py
+++ website/views.py
@@ -37,10 +37,72 @@ def addition(request):
     })
 
 def subtraction(request):
-    return render(request, 'subtraction.html',{})
+    from random import randint 
+    
+    number1 = randint(0,20)
+    number2 = randint(0,20)
+   
+    if request.method == "POST":
+       
+        answer = request.POST['answer']
+        old_number1=request.POST['old_number1']
+        old_number2=request.POST['old_number2']
+        correct_answer =int(old_number1) -int(old_number2)
+        if int(answer) == correct_answer:
+            my_answer = "Bravo, Right Answer " + old_number1 + "-" + old_number2 + "=" + answer
+            color ="primary"
+            
+        else:
+            my_answer="Incorrect " + old_number1 + "-" + old_number2 + "=" + str(correct_answer)
+            color ="danger"
+
+        return render(request,'subtraction.html',{
+            'answer':answer,
+            'my_answer':my_answer,
+            'number1':number1,
+            'number2':number2,
+            'color':color,
+            })
+ 
+    return render(request, 'subtraction.html',{
+        'number1':number1,
+        'number2':number2,
+    })
 
 def multiplication(request):
-    return render(request, 'multiplication.html',{})
+    from random import randint 
+    
+    number1 = randint(0,20)
+    number2 = randint(0,20)
+   
+    if request.method == "POST":
+       
+        answer = request.POST['answer']
+        old_number1=request.POST['old_number1']
+        old_number2=request.POST['old_number2']
+        correct_answer =int(old_number1) * int(old_number2)
+        if int(answer) == correct_answer:
+            my_answer = "Bravo, Right Answer " + old_number1 + "*" + old_number2 + "=" + answer
+            color ="primary"
+            
+        else:
+            my_answer="Incorrect " + old_number1 + "*" + old_number2 + "=" + str(correct_answer)
+            color ="danger"
+
+        return render(request,'multiplication.html',{
+            'answer':answer,
+            'my_answer':my_answer,
+            'number1':number1,
+            'number2':number2,
+            'color':color,
+            })
+ 
+    return render(request, 'multiplication.html',{
+        'number1':number1,
+        'number2':number2,
+    })
+
+
 
 def division(request):
     return render(request, 'division.html',{})
